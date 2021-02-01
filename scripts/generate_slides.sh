8a5c739f00d257854bd46c878db0b85d4d698dc1
Ethan Swan <eswan18@rocketmail.com>
2021-02-01 07:15:04 -0600

    Fix broken generate_slides script.

--- scripts/generate_slides.sh
+++ scripts/generate_slides.sh
@@ -1,3 +1,5 @@
+set -e
+
 if [ ! -d ".git" ]; then
     echo "Error: no .git directory detected"
     echo "This script should be run from the base of the repo"
@@ -11,5 +13,5 @@ cd notebooks
 cp -r images ../slides
 # Match all notebook files with content.
 for file in *-*.ipynb; do
-    jupyter nbconvert --to slides $file --reveal-prefix https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.3.0 --output-dir=../slides
+    jupyter nbconvert --to slides $file --output-dir=../slides
 done
